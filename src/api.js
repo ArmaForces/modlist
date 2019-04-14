5157ebac42453870e831e8f53e00253cb910a2d5
Filip Maciejewski <veteran29q@gmail.com>
2019-04-14 22:16:47 +0200

    Fix modset selection

--- src/api.js
+++ src/api.js
@@ -13,7 +13,7 @@ export const getDownloadableModsets = () =>
 
 
 export const getModsetData = modset =>
-  fetch(`${BASE_URL}/modsets/default.csv?${Date.now()}`)
+  fetch(`${BASE_URL}/modsets/${modset}.csv?${Date.now()}`)
   .then((resp) => resp.text())
   .then((csvText) => Papa.parse(csvText.trim(), {
     header: true,
