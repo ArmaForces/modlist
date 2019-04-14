bd8cd6ba1ac3b7e6531e9599dcbc3d83f86cda60
Filip Maciejewski <veteran29q@gmail.com>
2019-04-14 21:36:38 +0200

    Revert dist path

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ module.exports = {
   entry: ['babel-polyfill', './src/main.js'],
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/armaforces_www_modlist/dist/'
-    : '/dist/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
