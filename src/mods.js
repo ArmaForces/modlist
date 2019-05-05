c6cd226df34d9a9edd4bbe3adb3bd51b55e15803
Filip Maciejewski <veteran29q@gmail.com>
2019-05-06 01:56:29 +0200

    Optionals should be disabled by default

--- src/mods.js
+++ src/mods.js
@@ -1,6 +1,6 @@
 
-export const setEnabled = (id, state) => localStorage.setItem('ws_'+id, state)
+export const setEnabled = (id, state) => localStorage.setItem('mod_'+id, state)
 
-export const isEnabled = id => JSON.parse(localStorage.getItem('ws_'+id)) !== false
+export const isEnabled = id => JSON.parse(localStorage.getItem('mod_'+id)) === true
 
 export const isOptional = mod => mod.is_optional === 'True'
